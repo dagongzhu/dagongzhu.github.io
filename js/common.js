f02c540228bf574e3ed0706b99337f6680f4c1ec
dagongzhu <34031619+dagongzhu@users.noreply.github.com>
2018-01-05 23:54:42 +0800

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,39 @@
+var header = 
+    '<div class="header_cont">' +
+    '    <a class="logo" href="./index.html">明 德 妙 融 粉 扑</a>' +
+    '    <ul class="nav">' +
+    '        <li><a href="./index.html">首页</a></li>' +
+    '        <li><a href="./brandIntro.html">品牌介绍</a></li>' +
+    '        <li><a href="./product.html">明星产品</a></li>' +
+    '        <li><a href="./news.html">新闻动态</a></li>' +
+    '        <li><a href="./contact.html">联系我们</a></li>' +
+    '    </ul>' +
+    '</div>';
+
+$("#header").append(header);
+
+var footer =
+    '<div class="footer1">' +
+    '    <div class="footer1_cont clearfix">' +
+    '        <p class="txt1">始终以一颗追求完美的心，为企业插上腾飞的翅膀，高度精湛的生产制造工艺和近乎苛刻的产品质量监控，只为了用我们的专业服务更好地帮助客户创造实现更高的价值。</p>' +
+    '        <p class="txt2">Always with a perfect heart, and for the enterprise with wings of take-off, highly skilled manufacturing process and harsh judgment of product quality control, in order to use our professional services to better help customers create greater value.</p>' +
+    '        <div class="btns">' +
+    '            <a href="./brandIntro.html#brand_story">服务宗旨</a>' +
+    '            <a href="http://wpa.qq.com/msgrd?v=3&uin=441240278&site=qq&menu=yes" target="_blank">美妆咨询</a>' +
+    '        </div>' +
+    '    </div>' +
+    '</div>' +
+    '<div class="footer2">' +
+    '    <div class="footer2_cont">' +
+    '        <div class="nav_list">' +
+    '            <a href="./index.html">首页</a>' +
+    '            <a href="./brandIntro.html">品牌介绍</a>' +
+    '            <a href="./product.html">明星产品</a>' +
+    '            <a href="./news.html">新闻动态</a>' +
+    '            <a href="./contact.html">联系我们</a>' +
+    '        </div>' +
+    '        <div class="copyright">Copyright © 2017-2018 明德妙融粉扑 All Rights Reserved.</div>' +
+    '    </div>' +
+    '</div>';
+
+$("#footer").append(footer);
